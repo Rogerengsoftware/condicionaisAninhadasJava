e310fc55a83e8877f301288359155205b5ecc0a6
Roger Tavares <rogertavaresoperacional@gmail.com>
2024-10-01 12:32:05 -0300

    condicionais aninhadas.

---
+++ Aula028_condicionalAninhada/src/curso/condicionalAninhada.java
@@ -0,0 +1,17 @@
+package curso;
+
+public class condicionalAninhada {
+	
+	public static void main(String[] args) {
+	
+		int x = 7, y = 4;
+		
+		if (x > 5)
+			if (y > 5)
+				System.out.println("x e y são > 5");
+			else
+				System.out.println("x é <= 5");
+
+     }
+
+}
---
+++ Aula028_condicionalAninhada/src/curso/condicionalAninhada2.java
@@ -0,0 +1,24 @@
+package curso;
+
+public class condicionalAninhada2 {
+	
+	public static void main(String[] args) {
+	    int primeiro = 3;
+	    		int segundo = 3;
+	    		
+	    		if(primeiro == segundo) {
+	    			System.out.println("os números são iguais");
+	    		}
+	    		else {
+	    			if(primeiro > segundo) {
+	    				System.out.println("Primeiro é maior");
+	    			}
+	    		    else {
+	    				System.out.println("Segundo é maior");
+	    			}
+	    				
+	    	}
+	    		
+	}
+
+}
